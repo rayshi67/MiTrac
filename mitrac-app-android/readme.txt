6a64516ef39092fec71f8e0c45504d7b1ffb880c
rayshi67 <ray.b.shi@gmail.com>
2015-06-25 23:05:56 +1000

    added DeviceGroupDao

---
+++ mitrac-app-android/readme.txt
@@ -0,0 +1,5 @@
+Required changes when switching development environment
+1. Properties -> Android -> Set reference to the project google-play-services_lib
+2. update libs/android-support-v4.jar from Android SDK
+3. update MiTrac server IP and port in res/values/urls.xml
+4. Update Google Map API key in AndroidManifest.xml
---
+++ mitrac-server/src/main/java/com/jiesoft/mitrac/dao/DeviceGroupDao.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015 Jiesoft Consulting.
+ * All Rights Reserved.
+ *
+ * All information contained herein is, and remains the property
+ * of Jiesoft Consulting. The intellectual and technical concepts 
+ * contained herein are proprietary to Jiesoft and are protected by 
+ * trade secret or copyright law. Dissemination of this information 
+ * or reproduction of this material is strictly forbidden unless 
+ * prior written permission is obtained from Jiesoft Consulting.
+ *
+ *      http://www.jiesoft.com
+ */
+
+package com.jiesoft.mitrac.dao;
+
+import java.util.List;
+
+import com.jiesoft.mitrac.domain.bo.DeviceGroup;
+
+/**
+ * Device Group DAO interface.
+ * 
+ * @author Ray Shi
+ */
+
+public interface DeviceGroupDao {
+	
+	List<DeviceGroup> findDeviceGroupsByAccountName(String accountName);
+
+}
